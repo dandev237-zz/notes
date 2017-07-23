ee2d3dd88a661974dedd4897650f375d87faf695
Daniel Castro García <dandev237@gmail.com>
2017-07-23 12:19:18 +0200

    Added ExpectJS

---
+++ imports/api/users.test.js
@@ -0,0 +1,33 @@
+import expect from 'expect';
+
+const add = (a, b) => {
+    if (typeof b !== 'number') {
+        return a + a;
+    }
+
+    return a + b;
+};
+
+const square = (a) => a * a;
+
+describe('add', function () {
+    it('should add two numbers', function () {
+        const res = add(11, 9);
+
+        expect(res).toBe(20);
+    });
+
+    it('should double a single number', function () {
+        const res = add(44);
+
+        expect(res).toBe(88);
+    });
+});
+
+describe('square', function () {
+    it('should square a number', function () {
+        const res = square(11);
+
+        expect(res).toBe(121);
+    });
+});
